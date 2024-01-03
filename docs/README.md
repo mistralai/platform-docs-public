3c62001c0c33beef9720d75dcb23a70fe1cceb4e
Lélio <l@mistral.ai>
2024-01-03 16:33:24 +0100

    Fix search

--- docs/README.md
+++
@@ -1,1 +0,0 @@
-# mistral-docs
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,21 +155,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
     }),
-  themes: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        // language: ["en", "zh"],
-        // ```
-      }),
-    ],
-  ],
+    plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 module.exports = config;

341f0c47ba2485d1cdd5375c4a60ab7cec19bbdb
pandora <128635000+pandora-s-git@users.noreply.github.com>
2025-10-20 19:48:20 +0200

    update featured models

--- src/schema/content/models.ts
+++ src/schema/content/models.ts
@@ -2,21 +2,21 @@ import { ModelSlug } from '@/schema/models';
 
 export const FEATURED_MODEL_NAMES: ModelSlug[] = [
   'codestral-25-08',
+  'mistral-medium-3-1-25-08',
   'mistral-small-3-2-25-06',
-  'mistral-medium-3-25-05',
 ];
 
 export const LATEST_MODEL_NAMES: ModelSlug[] = [
+  'magistral-medium-1-2-25-09',
   'mistral-small-3-2-25-06',
   'codestral-25-08',
-  'magistral-medium-1-2-25-09',
   'voxtral-small-25-07',
 ];
 
 export const FEATURED_MODEL_NAMES_MODELS_PAGE: ModelSlug[] = [
   'codestral-25-08',
+  'mistral-medium-3-1-25-08',
   'mistral-small-3-2-25-06',
-  'mistral-medium-3-25-05',
 ];
 
 export const FEATURED_MODELS_COLOR_OVERRIDES = [
